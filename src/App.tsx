bbf52b3518ac6136404c156a3cc98114282eaeb8
xtc2016xtc <728611827@qq.com>
2024-09-30 17:50:08 +0800

    构建验证模块样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-
+import {Route, Routes} from 'react-router-dom'
 
 const App = () => {
     return (
         <>
-            {/*占比整个宽度*/}
+            {/*响应式整个屏幕占比宽度*/}
             <main className="flex h-screen">
-                1
+                <Routes>
+                    {/*登录注册*/}
+                    <Route>
+
+                    </Route>
+                </Routes>
             </main>
         </>
     )
